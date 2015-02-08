67d6cd4915f442659c54090bcf45e74bcda74eca
Luisita <luiyi_1414@hotmail.com>
2015-02-08 14:48:17 -0600

    3

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,3 @@
-// Evento device ready
 $(document).on('deviceready', function(){ 
-    var d = new Date();//Obtiene la fecha Actual
-    $("#fechaAct").html(d.toDateString(););
-    $.mobile.changePage("#fecha");
-      $.mobile.loading( "show", {
-          text: "Device Ready cargado",
-          textVisible: true,
-          theme: "e",
-          html: ""
-      });
+    alert("Listo el dispositivo");
 });
